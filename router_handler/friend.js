83dbaea48ce17e2262e43e12355801a262582ac1
Wendy <lienyun.chang@gmail.com>
2022-08-04 15:37:33 +0800

    addFriend非同步問題

--- router_handler/friend.js
+++ router_handler/friend.js
@@ -5,14 +5,13 @@ const getFriend = (req, res) => {
   const getFriendSql = 'SELECT user.user_id, user.email, friend.friend_id, friend.friend_displayname, friend.friend_pic FROM user LEFT JOIN friend ON user.user_id=friend.user_id WHERE user.user_id = ?'
   db.query(getFriendSql, req.session.user_id, (err, results) => {
     if (err) return res.cc(err)
-    console.log('getFriend', results)
+    // console.log('getFriend', results)
     res.send({
       status: 1,
       message: '朋友名單GET',
       data: results
     })
   })
-
 }
 
 //新增好友
@@ -23,34 +22,42 @@ const addFriend = (req, res) => {
   //沒有這個user
   const noUserSql = 'select * from user where email = ?'
   db.query(noUserSql, friendContent.email, (err, results) => {
+    console.log('friendContent.email', friendContent.email)
+    console.log('results', results)
     if (err) return console.log(err.message)
     if (results.length === 0) {
-      return res.cc('沒有這個user，請重新輸入')
+      return res.send({
+        status: 0,
+        message: '沒有這個user，請重新輸入！'
+      })
     }
-  })
 
-  //已經是好友了
+    //已經是好友了
   const friendNowSql = 'select * from friend where friend.friend_id = (select user.user_id from user where user.email = ?)'
   db.query(friendNowSql, friendContent.email, (err, results) => {
     if (err) return console.log(err.message)
     if (results.length > 0) {
-      return res.cc('已經是好友了，請重新輸入')
+      return res.send({
+        status: 0,
+        message: '已經是好友了，請重新輸入！'
+      })
     }
   })
-
-  //加好友
-  const addFriendSql = 'INSERT INTO friend(user_id, friend_id, friend_displayname, friend_pic) VALUES (?,(SELECT user.user_id FROM user WHERE user.email = ?),(SELECT user.displayname FROM user WHERE user.email = ?),(SELECT user.user_pic FROM user WHERE user.email = ?))'
-  db.query(addFriendSql, [req.session.user_id,friendContent.email,friendContent.email,friendContent.email], (err, results) => {
-
-    if (err) return res.cc(err)
-    console.log(err)
-    console.log('addFriend', results)
-    if (results.affectedRows === 1)
-      return res.send({
-        status: 1,
-        message: '新增好友成功！'
-      })
+    //加好友
+    const addFriendSql = 'INSERT INTO friend(user_id, friend_id, friend_displayname, friend_pic) VALUES (?,(SELECT user.user_id FROM user WHERE user.email = ?),(SELECT user.displayname FROM user WHERE user.email = ?),(SELECT user.user_pic FROM user WHERE user.email = ?))'
+    db.query(addFriendSql, [req.session.user_id, friendContent.email, friendContent.email, friendContent.email], (err, results) => {
+  
+      if (err) return res.cc(err)
+      console.log(err)
+      console.log('addFriend', results)
+      if (results.affectedRows === 1)
+        return res.send({
+          status: 1,
+          message: '新增好友成功！'
+        })
+    })
   })
+
 }
 
 module.exports = {
