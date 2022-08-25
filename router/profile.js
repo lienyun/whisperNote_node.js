1a47683a2b2a2e219b296f6d67256a830a21a7f2
Kelly Chang <u2kelly619@gmail.com>
2022-08-25 14:39:21 +0800

    profile API

--- router/profile.js
+++ router/profile.js
@@ -5,10 +5,10 @@ const router = express.Router()
 const profileHandler = require('../router_handler/profile')
 
 // 取得profile
-router.get('/profile', profileHandler.getProfile)
+router.get('/getProfile', profileHandler.getProfile)
 
 //更新profile
-router.post('./profile', profileHandler.updateProfile)
+router.post('/updateProfile', profileHandler.updateProfile)
 
 // 向外共享路由对象
 module.exports = router
