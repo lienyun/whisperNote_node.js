59547c9891b3ebf05b63a5895d1c1022cf1bd785
Wendy <lienyun.chang@gmail.com>
2022-08-25 23:25:18 +0800

    Revert "deleteCharacter"
    This reverts commit 981d3bd0a5ae68d1cdbdc9eafb75b2291cc63e01.

--- router_handler/profile.js
+++ router_handler/profile.js
@@ -14,15 +14,15 @@ const getProfile = (req, res) => {
 }
 //更新profile
 const updateProfile = (req, res) => {
-  const updateContent = req.body
+  const profileContent = req.body
   const sql = 'update user set ? where id = ?'
-  db.query(sql, [updateContent, req.session.uder_id],(err, results) =>{
+  db.query(sql, [profileContent, req.session.user_id],(err, results) =>{
     if (err) return console.log(err.message)
     if (results.length !== 1) return res.cc('修改用戶資訊失敗')
     res.send('更新用戶資訊成功', 0)
 
-  })
-}
+//   })
+// }
 
 module.exports = {
   getProfile,
